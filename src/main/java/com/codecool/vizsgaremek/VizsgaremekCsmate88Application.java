bb46464e795086afd5b123c1c45e3fd8b4085824
csmate88 <csallo.mate@gmail.com>
2022-06-20 09:30:44 +0200

    Starter code

---
+++ src/main/java/com/codecool/vizsgaremek/VizsgaremekCsmate88Application.java
@@ -0,0 +1,13 @@
+package com.codecool.vizsgaremek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizsgaremekCsmate88Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VizsgaremekCsmate88Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/vizsgaremek/VizsgaremekCsmate88ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.vizsgaremek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizsgaremekCsmate88ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
