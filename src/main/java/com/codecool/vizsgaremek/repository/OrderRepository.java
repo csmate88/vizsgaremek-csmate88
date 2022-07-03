5419b05bd551034222bb952caa99267ce6c98817
csmate88 <csallo.mate@gmail.com>
2022-07-03 20:09:08 +0200

    Add findOrdersBeforeDate query

--- src/main/java/com/codecool/vizsgaremek/repository/OrderRepository.java
+++ src/main/java/com/codecool/vizsgaremek/repository/OrderRepository.java
@@ -2,6 +2,12 @@ package com.codecool.vizsgaremek.repository;
 
 import com.codecool.vizsgaremek.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query(value = "select o from Order o where o.orderTime<=?1")
+    List<Order> findOrdersBeforeDate(LocalDateTime date);
 }
