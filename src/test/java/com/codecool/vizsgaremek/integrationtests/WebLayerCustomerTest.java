00309e5fa15e8c68d9e3eca1e5f8ba855159c481
csmate88 <csallo.mate@gmail.com>
2022-07-05 12:40:15 +0200

    Add additional test classes

---
+++ src/test/java/com/codecool/vizsgaremek/integrationtests/WebLayerCustomerTest.java
@@ -0,0 +1,79 @@
+package com.codecool.vizsgaremek.integrationtests;
+
+
+import com.codecool.vizsgaremek.TestUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class WebLayerCustomerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void clean() {
+        TestUtil.resetDatabase();
+    }
+
+
+    @Test
+    void getRequestCustomer_ReturnsJSON() throws Exception{
+        mockMvc.perform(get("/customer")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getRequest_ReturnsAListOfAllCustomers() throws Exception{
+        mockMvc.perform(get("/customer").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(5)));
+    }
+
+
+    @Test
+    void findCustomer_ReturnsSpecificCustomer_GivenId() throws Exception{
+        mockMvc.perform(get("/customer/5")).andExpect(status().isOk()).andExpect(content().json("{\"id\":5,\"name\":\"Tim Doe\",\"email\":\"tim.doe@email.fr\",\"telephoneNumber\":\"+36337689130\",\"address\":\"3000 Hatvan Puszta tér 5.\",\"orders\":[]}"));
+    }
+
+
+
+    @Test
+    void getRequestCustomer_ReturnsNotFoundNot_WhenIndexOutOfBounds() throws Exception{
+        mockMvc.perform(get("/customer/100").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void findCustomerByIdAndDelete_CustomerNotFoundAfterDelete() throws Exception{
+        mockMvc.perform(get("/customer/3").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":3,\"name\":\"Joe Doe\",\"email\":\"john.doe@email.it\",\"telephoneNumber\":\"+36229999999\",\"address\":\"5000 Szolnok Valami utca 12.\",\"orders\":[{\"id\":4,\"products\":[{\"id\":4,\"name\":\"Bacon\",\"description\":\"Meat\"},{\"id\":10,\"name\":\"Energy Drink\",\"description\":\"Sugar and caffeine\"}],\"orderTime\":\"2020-05-30T18:45:00\"}]}"));
+        mockMvc.perform(delete("/customer/3"));
+        mockMvc.perform(get("/customer/3")).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void saveCustomer_ReturnSavedCustomer() throws Exception {
+
+    }
+
+    @Test
+    void UpdateCustomerChanges() throws Exception{
+
+    }
+
+
+
+}
---
+++ src/test/java/com/codecool/vizsgaremek/integrationtests/WebLayerProductTest.java
@@ -0,0 +1,69 @@
+package com.codecool.vizsgaremek.integrationtests;
+
+import com.codecool.vizsgaremek.TestUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class WebLayerProductTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void clean() {
+        TestUtil.resetDatabase();
+    }
+
+    @Test
+    void getRequestProduct_ReturnsJSON() throws Exception{
+        mockMvc.perform(get("/product")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getRequest_ReturnsAListOfAllProducts() throws Exception{
+        mockMvc.perform(get("/product").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(11)));
+    }
+
+    @Test
+    void findProduct_ReturnsSpecificProduct_GivenId() throws Exception{
+        mockMvc.perform(get("/product/4")).andExpect(status().isOk()).andExpect(content().json("{\"id\":4,\"name\":\"Bacon\",\"description\":\"Meat\"}"));
+    }
+
+    @Test
+    void getRequestProduct_ReturnsNotFoundNot_WhenIndexOutOfBounds() throws Exception{
+        mockMvc.perform(get("/product/100").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void findProductByIdAndDelete_ProductNotFoundAfterDelete() throws Exception{
+        mockMvc.perform(get("/product/3").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":3,\"name\":\"Energy Drink\",\"description\":\"Sugar and caffeine\"}"));
+        mockMvc.perform(delete("/product/3"));
+        mockMvc.perform(get("/product/3")).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void saveProduct_ReturnSavedProduct() throws Exception {
+        
+    }
+
+    @Test
+    void UpdateProductChanges() throws Exception{
+
+    }
+}
